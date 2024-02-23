e2776000e8a2ba24d18b664dc15ca55c360404ed
Friezer <hello@friezer.tech>
2024-02-23 03:00:34 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,17 +4,17 @@ set -o errexit
 declare -r currentDir="$(dirname "${BASH_SOURCE[0]}")"
 source "${currentDir}/build.properties"
 
-DISTRIBUTION_NAME=verlainesso-theme-$THEME_VERSION
+DISTRIBUTION_NAME=spherelink-theme-$THEME_VERSION
 
 # prepare and zip the theme content
 echo "info::: Removing an existing '$DISTRIBUTION_NAME.zip' file."
 rm -rf $DISTRIBUTION_NAME.zip
 
 mkdir alfresco
-echo "info::: Packaging verlainesso theme as '$DISTRIBUTION_NAME.zip'"
+echo "info::: Packaging spherelink theme as '$DISTRIBUTION_NAME.zip'"
 
 cp -rf theme/* verlainesso/
-zip -r $DISTRIBUTION_NAME.zip verlainesso
+zip -r $DISTRIBUTION_NAME.zip spherelink
 
 echo "info::: Cleanup temp files and folders."
-rm -rf verlainesso
+rm -rf spherelink
